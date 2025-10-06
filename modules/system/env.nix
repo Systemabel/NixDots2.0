65c8f7cb0f4e4e298f640599fb48d241f2cd8cd6
Systemabel <hsabel@outlook.com>
2025-10-05 21:16:01 -0500

    Adjust env vars

--- modules/system/env.nix
+++ modules/system/env.nix
@@ -1,11 +1,22 @@
 {lib, ...}: {
-  environment.sessionVariables = lib.mkDefault {
-    # Wayland settings
-    NIXOS_OZONE_WL = "1";
-    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+  environment = {
+    # variables =
+    #   lib.mkDefault {
+    #   };
+    sessionVariables = lib.mkDefault {
+      # Wayland settings
+      NIXOS_OZONE_WL = "1";
+      ELECTRON_OZONE_PLATFORM_HINT = "wayland";
 
-    # WAYLAND_DISPLAY = "wayland-1";
-    TERM = "ghostty";
-    BROWSER = "firefox";
+      # WAYLAND_DISPLAY = "wayland-1";
+      # Defaults
+      TERM = "ghostty";
+      BROWSER = "firefox";
+      EDITOR = "nano";
+
+      # Setting config location variables:
+      # XDG_CONFIG_HOME = "/home/";
+      # ZDOTDIR = "$XDG_CONFIG_HOME/zsh";
+    };
   };
 }
